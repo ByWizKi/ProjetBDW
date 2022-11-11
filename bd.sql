0ed71c15ad6cd43af104d3c497294b5362bef9d1
Enzo PC <enzoth39260@gmail.com>
2022-11-11 18:47:23 +0100

    Debut de l'insertion de qlq donnees dans la bd

--- bd.sql
+++ bd.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Groupe (
     identifiantGroupe INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
     nomGroupe VARCHAR(255), 
-    dateCreationGroupe DATE, 
-    dateSeparationGroupe DATE);
+    dateCreationGroupe YEAR, 
+    dateSeparationGroupe YEAR);
 
 
 CREATE TABLE Musicien(
@@ -34,7 +34,7 @@ CREATE TABLE Album(
 CREATE TABLE Playlist( 
     identifiantPlaylist INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nomPlaylist VARCHAR(255),
-    dateCreationPlaylist DATE DEFAULT CURRENT_TIMESTAMP);
+    dateCreationPlaylist DATETIME DEFAULT CURRENT_TIMESTAMP);
 
 CREATE TABLE Chanson(
     identifiantChanson INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -80,8 +80,8 @@ CREATE TABLE FaitParti(
     identifiantGroupe INT,
     identifiantMusicien INT,
     roleGroupe VARCHAR(255),
-    dateEntrerGroupe DATE,
-    dateSortieGroupe DATE,
+    dateEntrerGroupe YEAR,
+    dateSortieGroupe YEAR,
     membreFondateur INT CHECK (membreFondateur = 0 OR membreFondateur = 1),
     PRIMARY KEY(identifiantGroupe, identifiantMusicien),
     FOREIGN KEY(identifiantGroupe) REFERENCES Groupe(identifiantGroupe),
@@ -151,5 +151,55 @@ CREATE TABLE RelationChanson(
 
 /* INSERTION */
 
-INSERT INTO `Album` (`identifiantAlbum`, `libelleAlbum`, `titreAlbum`, `dateSortieAlbum`, `nomProducteur`, `nomIngenieurSon`, `descriptionAlbum`) VALUES (NULL, 'AlbumStudio', 'Elevation', '2022-11-11', 'BEP Music', 'Michel Son', NULL);
-INSERT INTO 'Album' ('identifiantAlbum', 'libelleAlbum', 'titreAlbum', 'dateSortieAlbum', 'nomProducteur', 'nomIngenieurSon', 'descriptionAlbum') VALUES (NULL, 'AlbumCompilation', 'The Beatles', '1968-11-22', 'Calderstone Productions Limited', NULL, 'Double Album Du groupe The Beatles');
+INSERT INTO Album (identifiantAlbum, libelleAlbum, titreAlbum, dateSortieAlbum, nomProducteur, nomIngenieurSon, descriptionAlbum) VALUES (NULL, 'AlbumStudio', 'Elevation', '2022-11-11', 'BEP Music', 'Michel Son', NULL);
+INSERT INTO Album (identifiantAlbum, libelleAlbum, titreAlbum, dateSortieAlbum, nomProducteur, nomIngenieurSon, descriptionAlbum) VALUES (NULL, 'AlbumCompilation', 'The Beatles', '1968-11-22', 'Calderstone Productions Limited', NULL, 'Double Album Du groupe The Beatles');
+INSERT INTO Album (identifiantAlbum, libelleAlbum, titreAlbum, dateSortieAlbum, nomProducteur, nomIngenieurSon, descriptionAlbum) VALUES (NULL, 'AlbumStudio', 'Ecole des points vitaux', '2010-03-29', 'WHATI-B', 'Renaud Son', NULL);
+
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL,'Ann Ferguson', 'Stacy', 'Fergie');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'James Adams', 'William ', 'Will.i.am');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Pineda Lindo', 'Allen', 'Apl.de.ap');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Hill', 'Kim', 'Kim Hill');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Luis Gomez', 'James', 'Taboo');
+
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Lennon', 'Jhon', 'Jhon Lennon');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'McCartney', 'Paul', 'Fergie');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Starkey', 'Richard', 'Ringo Starr');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Harrison', 'George', 'George Harrison');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Fergusson', 'Stuart', 'Stuart Sutcliffe');
+
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Djuna', 'Gandhi', 'Maître Gims');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Diallo', 'Adama', 'Barack Adama');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Diallo', 'Alpha', 'Black M');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Ballo', 'Karim', 'JR O Crom');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Vincent', 'Bastien', 'Maska');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Baldé', 'Mamadou', 'Doomams');
+INSERT INTO Musicien (identifiantMusicien, nomMusicien, prenomMusicien, nomScene) VALUES (NULL, 'Fall', 'Abdel Karim', 'Lefa'); 
+
+INSERT INTO Groupe (identifiantGroupe, nomGroupe, dateCreationGroupe, dateSeparationGroupe) VALUES (NULL, 'Black Eyed Peas', '1995', NULL);
+INSERT INTO Groupe (identifiantGroupe, nomGroupe, dateCreationGroupe, dateSeparationGroupe) VALUES (NULL, 'The Beatles', '1962', '1970');
+INSERT INTO Groupe (identifiantGroupe, nomGroupe, dateCreationGroupe, dateSeparationGroupe) VALUES (NULL, 'Sexion d Assaut', '2002', '2013');
+
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (1, 1, 'Chanteuse', '2003', '2010', 0);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (1, 2, 'Chanteure', '1995', NULL, 1);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (1, 3, 'Chanteure', '1995', NULL, 1);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (1, 4, 'Chanteuse', '1995', '2000', 0);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (1, 5, 'Chanteure', '1995', NULL, 0);
+
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (2, 6, 'Guitariste', '1962', '1970', 1);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (2, 7, 'Bassiste', '1962', '1970', 1);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (2, 8, 'Batteur', '1962', '1970', 0);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (2, 9, 'Guitariste', '1962','1970', 0);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (2, 10, 'Guitariste, Bassiste', '1962','1970', 0);
+
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (3, 11, 'Rappeur', '2002','2013', 1);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (3, 12, 'Rappeur', '2002','2013', 1);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (3, 13, 'Rappeur', '2002','2013', 1);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (3, 14, 'Rappeur', '2002','2013', 1);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (3, 15, 'Rappeur', '2002','2013', 1);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (3, 16, 'Rappeur', '2002','2013', 1);
+INSERT INTO FaitParti (identifiantGroupe, identifiantMusicien, roleGroupe, dateEntrerGroupe, dateSortieGroupe, membreFondateur) VALUES (3, 17, 'Rappeur', '2002','2013', 1);
+
+
+
+
+
