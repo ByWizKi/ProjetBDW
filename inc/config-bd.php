23db0d4d4369a17468d8df6b801846478694d60f
Enzo PC <enzoth39260@gmail.com>
2022-11-16 12:53:51 +0100

    ajout de quelque fonction php pour recup des infos

--- inc/config-bd.php
+++ inc/config-bd.php
@@ -3,8 +3,8 @@ if(file_exists('../private-config-bd.php'))
 	require('../private-config-bd.php');
 else {
 	define('SERVEUR', 'localhost');
-	define('UTILISATRICE', 'localhost'); 
-	define('MOTDEPASSE', ''); 
-	define('BDD', 'test'); 
+	define('UTILISATRICE', 'p2207446'); 
+	define('MOTDEPASSE', 'Seduce65Maybe'); 
+	define('BDD', 'p2207446'); 
 }
 ?>
--- modele/modele.php
+++ modele/modele.php
@@ -1,5 +1,5 @@
 <?php
-
+// Connexion base de donnees
 function getConnexionBD() {
 	$connexion = mysqli_connect(SERVEUR, UTILISATRICE, MOTDEPASSE, BDD);
 	if (mysqli_connect_errno()) {
@@ -10,10 +10,86 @@ function getConnexionBD() {
 	return $connexion;
 }
 
+//Deconnexion base de donnees
 function deconnectBD($connexion) {
 	mysqli_close($connexion);
 }
 
+//nombre d'element dans une table
 
+function nombreElementTable($connexion, $nomTable){
+	$requete = "SELECT COUNT(*) AS nbElementTable From $nomTable";
+	$res = mysqli_query($connexion, $requete);
+	if ($res != true){
+		$row = mysqli_fetch_row($res);
+		return $row['nb'];
+
+	} 
+	return "Erreur la table n'existe pas";
+
+}
+
+function topElementChanson($connexion){
+
+	$requete = "SELECT titreChanson AS titre From Chanson INNER JOIN FichierAudio ON Chanson.identifiantChanson = FichierAudio.identifiantChanson ORDER BY FichierAudio.playCount ASC";
+	$res = mysqli_query($connexion, $requete);
+	$sortRow = mysqli_fetch_all($res, MYSQLI_ASSOC);
+	$resFinal = array();
+	for($i = 0; $i < 5; $i+=1){
+		$resFinal[$i] = $sortRow[$i]; 
+	}
+	return $resFinal;
+}
+
+function topElementGenre($connexion){
+
+	$requete = "SELECT nomGenre AS nom FROM Genre NATURAL JOIN APourGenre NATURAL JOIN Chanson NATURAL JOIN FichierAudio GROUP BY Genre.nomGenre ORDER BY FichierAudio.playCount ASC";
+	$res = mysqli_query($connexion, $requete);
+	$sortRow = mysqli_fetch_all($res, MYSQLI_ASSOC);
+	$resFinal = array();
+	for($i = 0; $i < 5; $i+=1){
+		$resFinal[$i] = $sortRow[$i]; 
+	}
+	return $resFinal;
+}
+
+function topElementAlbum($connexion){
+
+	$requete = "SELECT titreAlbum FROM Album NATURAL JOIN Chanson NATURAL JOIN FichierAudio GROUP BY titreAlbum ORDER BY FichierAudio.playCount ASC";
+	$res = mysqli_query($connexion, $requete);
+	$sortRow = mysqli_fetch_all($res, MYSQLI_ASSOC);
+	$resFinal = array();
+	for($i = 0; $i < 5; $i+=1){
+		$resFinal[$i] = $sortRow[$i]; 
+	}
+	return $resFinal;
+}
+
+function informationTable($connexion, $nomTable){
+	if ($nomTable = "Chanson"){
+		$requete = "SELECT titreChanson nomGroupe, dureeVersion, libelleVersion From Chanson NATURAL JOIN Groupe NATURAL JOIN FichierAudio";
+	}
+	elseif($nomTable = "Groupe"){
+		$requete = "SELECT nomGroupe, COUNT(titreChanson), dateCreationGroupe FROM Groupe NATURAL JOIN Chanson GROUP BY nomGroupe";
+	}
+
+	elseif($nomTable = "Genre"){
+		$requete = "SELECT DISTINCT nomGenre FROM Genre";
+	}
+
+	elseif($nomTable = "Album"){
+		$requete = "SELECT titreAlbum, COUNT(identifiantchanson), dateSortieAlbum FROM Album NATURAL JOIN Enregistre NATURAL JOIN Groupe NATURAL JOIN Chanson GROUP BY titreAlbum";
+	}
+	else{
+		$requete = "SELECT * FROM $nomTable";
+	}
+
+	$res = mysqli_query($connexion, $requete);
+	$infoTable = mysqli_fetch_all($res, MYSQLI_ASSOC);
+	return $infoTable;
+}
+
+
+		
 
 ?>
