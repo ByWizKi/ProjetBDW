740d38bf869a5f0c85ee4de53bd0a7748da6c79b
Enzo PC <enzoth39260@gmail.com>
2022-11-17 21:46:46 +0100

    update

--- static/menu.php
+++ static/menu.php
@@ -3,7 +3,7 @@
 
         <div id="textMenu">
             <a href="index.php" id="t1M">Accueil</a>
-            <a href="vues/vueAjouterChanson.php" id="t3M">Ajouter chanson</a>
+            <a href="vues/vueAjouterChanson.php" id="t3M">Ajouter Chanson</a>
             <a href="vues/vueImport.php" id="t3M">Les Importations</a>
             <a href="vues/vueAjouterPlaylist.php"id="t4M">Ajouter Playlist</a>
         </div>
