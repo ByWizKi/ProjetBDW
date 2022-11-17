c0f186a905d423e080165bd7659edfa69fe5b4bc
Enzo PC <enzoth39260@gmail.com>
2022-11-17 22:27:55 +0100

    update

--- static/footer.php
+++ static/footer.php
@@ -1,6 +1,6 @@
 <footer>
 	<div id="ImgFoot">
-		<a href="https://creativecommons.org/licenses/" target="_blank"><img src="img\cc.logo.large.png" alt="Licence CC BY-NC-SA"/></a>
+		<a href="https://creativecommons.org/licenses/" target="_blank"><img src="img\" alt="Licence CC BY-NC-SA"/></a>
 	</div>
 	<span><?php print date("Y"); ?> - <em><?= $nomSite ?></em></span>
 	<span><a href="https://perso.liris.cnrs.fr/fabien.duchateau/bdw" target="_blank" alt="Page BDW1">BDW - Base de données et programmation web</a> - UCB Lyon 1</span>
