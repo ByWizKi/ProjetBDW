e2b5886bab12119c6a44f258496004fd5d35b67a
marc <marcyrius6@gmail.com>
2022-11-18 13:02:21 +0100

    k

--- inc/config-bd.php
+++ inc/config-bd.php
@@ -3,8 +3,8 @@ if(file_exists('../private-config-bd.php'))
 	require('../private-config-bd.php');
 else {
 	define('SERVEUR', 'localhost');
-	define('UTILISATRICE', 'p2207446'); 
-	define('MOTDEPASSE', 'Seduce65Maybe'); 
-	define('BDD', 'p2207446'); 
+	define('UTILISATRICE', 'root'); 
+	define('MOTDEPASSE', ''); 
+	define('BDD', 'test'); 
 }
 ?>
--- static/recherche.php
+++ static/recherche.php
@@ -1,6 +1,20 @@
 <div id="divSearch">
-    <img src="img/searchIcon" alt="">
-    <input id ="inputSearch" type="search" placeholder="Recherche">
+    <!--<div id="searchIcon">
+        <img
+        src="img/loupe.png"
+        alt="loupe"
+        height="87px"
+        width="87px" />
+        
+    </div>
+    <div id=line></div>-->
+
+    <div id="inputSearch">
+    <form title="Recherche">
+    <input id="inputSearch" type="search" placeholder="Rechecher...">
+    <button type="submit">Recherche</button>
+    </form>
+</div>
     <label for="selectFiltres" id="selectFiltreText">Filtres</label>
     <select name="selectFiltre" id="selectFiltres">
     </select>
