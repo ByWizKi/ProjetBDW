11d5a84314909632bf1b45d85a292e892e3b1eb9
Enzo PC <enzoth39260@gmail.com>
2022-11-21 17:45:26 +0100

    update

--- vues/vueAjouterChanson.php
+++ vues/vueAjouterChanson.php
@@ -4,11 +4,21 @@
         <form method="POST" action="#">
             <label for=""></label>
             <input type="text" name="inputTitre" id="inputTitre">
-            <input type="text">
-            <input type="text">
-            <input type="text">
-            <input type="text">
-            <input type="text">
+
+            <label for=""></label>
+            <select name="selectGroupe" id="selectGroupe"></select>
+
+            <label for=""></label>
+            <input type="time" name="inputTime" id="inputTime">
+
+            <label for=""></label>
+            <select name="selectGenre" id="selectGenre"></select>
+
+            <label for=""></label>
+            <input type="date" name="inputDate" id="inputDate">
+
+            <label for=""></label>
+            <input type="checkbox">
         </form>
     </div>   
 </main>
