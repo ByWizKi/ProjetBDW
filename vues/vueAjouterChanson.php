5e95d17eb567f2ab37c1879bfc8e9aca9bc9e99f
Enzo PC <enzoth39260@gmail.com>
2022-11-21 17:45:52 +0100

    update

--- vues/vueAjouterChanson.php
+++ vues/vueAjouterChanson.php
@@ -2,23 +2,23 @@
     <div>
         <h1>Ajouter Une Chanson</h1>
         <form method="POST" action="#">
-            <label for=""></label>
+            <label for="inputTitre"> Titre</label>
             <input type="text" name="inputTitre" id="inputTitre">
 
-            <label for=""></label>
+            <label for="selectGroupe">Groupe</label>
             <select name="selectGroupe" id="selectGroupe"></select>
 
-            <label for=""></label>
+            <label for="inputTime">Duree</label>
             <input type="time" name="inputTime" id="inputTime">
 
-            <label for=""></label>
+            <label for="selectGenre">Genre</label>
             <select name="selectGenre" id="selectGenre"></select>
 
-            <label for=""></label>
+            <label for="inputDate">Date De Sortie</label>
             <input type="date" name="inputDate" id="inputDate">
 
-            <label for=""></label>
-            <input type="checkbox">
+            <label for="inputCheckBox">Date Aujourd'hui</label>
+            <input type="checkbox" id="inputCheckBox" name="inputCheckBox">
         </form>
     </div>   
 </main>
