399bb2d20aeb6bbbc7772fa71d6a01908459aee7
Enzo PC <enzoth39260@gmail.com>
2022-11-21 17:40:56 +0100

    update

---
+++ vues/vueAjouterChanson.php
@@ -0,0 +1,14 @@
+<main>
+    <div>
+        <h1>Ajouter Une Chanson</h1>
+        <form method="POST" action="#">
+            <label for=""></label>
+            <input type="text" name="inputTitre" id="inputTitre">
+            <input type="text">
+            <input type="text">
+            <input type="text">
+            <input type="text">
+            <input type="text">
+        </form>
+    </div>   
+</main>
