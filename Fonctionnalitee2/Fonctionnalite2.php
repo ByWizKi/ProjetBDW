eeecf858fbdf40446a299cb459e7e6ac28d399c3
Enzo PC <enzoth39260@gmail.com>
2022-11-29 17:19:11 +0100

    albumExist fini

--- Fonctionnalitee2/Fonctionnalite2.php
+++ Fonctionnalitee2/Fonctionnalite2.php
@@ -1,7 +1,13 @@
 <?php
 
+//connexion aux bases de donnees
+$connexionBD = mysqli_connect('localhost', 'p2207446', 'Seduce65Maybe', 'dataset');
+$connexion = mysqli_connect('localhost', 'p2207446', 'Seduce65Maybe', 'p2207446');
 
-$connexion = ;
+//recuperation des donnees de la table songs
+$requeteForSong = "SELECT * FROM songs100";
+$resForSong = mysqli_query($connexionBD, $requeteForSong);
+$tableSong = mysqli_fetch_all($resForSong, MYSQLI_ASSOC);
 
 // Test si un groupe existe 
 function groupeExsit($connexion, $nomGroupe){
@@ -43,10 +49,36 @@ function AjoutGroupe($connexion, $nomGroupe){
     }
 }
 
-function main($connexion, $data){
-    
+function albumExist($connexion, $nomGroupe, $titreAlbum, $compilation){
+    $nomGroupe = mysqli_real_escape_string($connexion, $nomGroupe);
+    $titreAlbum = mysqli_real_escape_string($connexion, $titreAlbum);
+    if($compilation != 0){
+        $requete = "SELECT nomAlbum FROM Album NATURAL JOIN Enregistre NATURAL JOIN Groupe WHERE '$nomGroupe' = nomGroupe AND '$titreAlbum' = titreAlbum AND (libelleAlbum = 'AlbumStudio' OR libelleAlbum = 'Albumlives')";
+    }
+    else{
+        $requete = "SELECT nomAlbum FROM Album NATURAL JOIN Enregistre NATURAL JOIN Groupe WHERE '$nomGroupe' = nomGroupe AND '$titreAlbum' = titreAlbum AND libelleAlbum = 'AlbumCompilation'";
 
+    }
+    $res = mysqli_query($connexion, $requete);
+    if($res != false)
+    {
+        $resultat = mysqli_fetch_all($res, MYSQLI_ASSOC);
+        if (count($resultat) == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    return -1;
+}
 
+function main($connexion, $datas){
+    foreach($datas as $data){
+        AjoutGroupe($connexion, $data['artist']);
+    }   
 }
 
 
