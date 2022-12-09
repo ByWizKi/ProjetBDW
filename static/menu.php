9e00cd4fdb62f8e479b41a0d9e17511dd8a9b84a
Enzo PC <enzoth39260@gmail.com>
2022-12-09 08:59:47 +0100

    oui

--- static/menu.php
+++ static/menu.php
@@ -4,7 +4,7 @@
             <a href="index.php" id="t1M">Accueil</a>
             <a href="index.php?page=AjouterChanson" id="t3M">Ajouter Chanson</a>
             <a href="index.php?page=AjouterPlaylist"id="t4M">Ajouter Playlist</a>
-            <a href="index.php?page=ModificationPlaylist" id="t3M">ModificationPlaylist</a>
+            <a href="index.php?page=ModificationPlaylist" id="t3M">Modification Playlist</a>
         </div>
     </nav>
 </div>
